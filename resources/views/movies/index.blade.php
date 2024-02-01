ef62490272c2d744e4b0a266653c39449f7512f7
UmbertoSerino <serumberto@gmail.com>
2024-02-01 22:14:29 +0100

    create movie cards in index

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,11 +1,31 @@
 @extends('layouts.app')
 @section('main-content')
 <h1>
-    Index.blade.php
+    I miei film preferiti del 1900
 </h1>
-
-<?php 
-var_dump($movies)
-?>
-
+        @foreach ($movies as $movie )
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Titolo: 
+                    {{ $movie['title'] }}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">
+                    @if ($movie['title'] != $movie['original_title'])
+                    <strong>Titolo Originale:</strong> {{ $movie['original_title'] }}
+                    @endif
+                </h6>
+                <h6 class="card-subtitle mb-2 text-muted">
+                    Nazionalità:
+                    {{ $movie['nationality'] }}
+                </h6>
+                <h6 class="card-subtitle mb-2 text-muted">
+                    Anno uscita:
+                    {{ $movie['date'] }}
+                </h6>
+                <h6 class="card-subtitle mb-2 text-muted">
+                    Media voto:
+                    {{ $movie['vote'] }}
+                </h6>
+            </div>
+          </div>
+    @endforeach
 @endsection
